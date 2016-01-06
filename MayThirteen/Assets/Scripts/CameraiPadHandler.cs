7d59708bc8c8025fab74e2da57f1f0bb5f995911
Jturesson90 <Jturesson90@gmail.com>
2016-01-06 16:24:38 +0100

    IOS Ready

---
+++ MayThirteen/Assets/Scripts/CameraiPadHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraiPadHandler : MonoBehaviour
+{
+
+	// Use this for initialization
+	public bool NeedFixForIpad = false;
+
+	void Awake ()
+	{
+		if (!NeedFixForIpad)
+			return;
+		float x = transform.position.x;
+		float y = transform.position.y;
+		float z = transform.position.z;
+		float aspect = ((float)Screen.height) / (float)Screen.width;
+		print ("Aspect = " + aspect);
+		if (aspect > 0.7f) {
+			z -= 13.5f;
+		} else if (aspect > 0.6f) {
+			z -= 10f;
+		}
+		transform.position = new Vector3 (x, y, z);
+	}
+	
+	// Update is called once per frame
+}
---
+++ MayThirteen/Assets/Scripts/OnlyAndroid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OnlyAndroid : MonoBehaviour
+{
+    public Sprite IOS_Sprite;
+    // Use this for initialization
+    void Awake()
+    {
+#if !UNITY_ANDROID
+        //gameObject.SetActive (false);
+        var rectTransform = GetComponent<RectTransform>();
+        rectTransform.offsetMin = new Vector2(20f, 20f);
+        rectTransform.offsetMax = new Vector2(-20f, -20f);
+        var image = GetComponent<Image>();
+        image.sprite = IOS_Sprite;
+        
+#endif
+
+    }
+}
