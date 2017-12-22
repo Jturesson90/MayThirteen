1614431a0db2ad639db4238b925ab982932b5287
Jesper Turesson <Jturesson90@gmail.com>
2017-12-22 09:45:23 +0100

    Some Refactoring

---
+++ MayThirteen/Assets/Scripts/Constants/RockstarScenes.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Drolegames.LittleRockstar.Scenes.Constants
+{
+
+    public static class RockstarScenes
+    {
+
+        public const string Menu = "Menu";
+        public const string Tutorial = "IntroScene";
+        public const string Lobby = "SelectionLobby";
+    }
+}
+
